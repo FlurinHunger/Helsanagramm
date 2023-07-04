77945cebad50567c305ea67ec1405e04de8bce82
FlurinHunger <47223054+FlurinHunger@users.noreply.github.com>
2023-07-04 18:30:55 +0200

    idk

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
     projectId: 'helsanagramm',
     appId: '1:14440237821:web:a69f9f66ab31c662c50c94',
     storageBucket: 'helsanagramm.appspot.com',
+    locationId: 'europe-west6',
     apiKey: 'AIzaSyDyykkTLwxGOHfvi40RlygrFoMIe5g_NW4',
     authDomain: 'helsanagramm.firebaseapp.com',
     messagingSenderId: '14440237821',
